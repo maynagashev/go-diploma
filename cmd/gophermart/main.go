b7f568f99c59b1b34299fd7ef39334d1f12dd56f
Eugene Mainagashev <lifesup@gmail.com>
2024-06-07 11:32:02 +0700

    Настройка тестов gophermarttest и докер композ для локальной работы

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import "net/http"
+
+func main() {
+
+	// обработчик запросов
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// отправка ответа
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, Gopher!"))
+	})
+
+	// старт сервера на порту 8080 без маршрутов и ответом 200 OK
+	http.ListenAndServe(":8080", nil)
+}
