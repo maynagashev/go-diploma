ffd63a259fddeb919e02c408119b63e895bd966d
Maynagashev <lifesup@gmail.com>
2025-01-13 12:47:20 +0700

    Инициализация логгера

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	initLogger()
 	port := "8080"
 
-	// обработчик запросов
+	// Обработчик запросов
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// отправка ответа
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello, Gopher!"))
 	})
 
-	// старт сервера с выводом информации
-	log.Printf("Starting server on :%s", port)
+	// Старт сервера с выводом информации
+	slog.Info("Starting server", "port", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal("Server error:", err)
+		slog.Error("Server error", "error", err)
+		os.Exit(1)
 	}
+}
+
+func initLogger() {
+	// Создаем переменную для уровня логирования и устанавливаем ее в Info
+	logLevel := new(slog.LevelVar)
+	logLevel.Set(slog.LevelDebug)
+
+	// Создаем новый обработчик с настроенным уровнем логирования
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+
+	// Устанавливаем созданный логгер как логгер по умолчанию
+	slog.SetDefault(logger)
 }
